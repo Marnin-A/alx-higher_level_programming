0e87f1a9e6a2cdf94aaf1cec232e5004d7e60bb9
Marnin-A <msmaudu@gmail.com>
2023-12-11 15:48:21 -0800

    Indentation🤦‍♂️

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -11,17 +11,17 @@ class Base:
      __nb_objects (int): The number of instantiated Bases.
      """
 
-      __nb_objects = 0
+    __nb_objects = 0
 
-       def __init__(self, id=None):
+    def __init__(self, id=None):
         """
         Initialize a new Base.
         Args:
             id (int): The identity of the new Base.
         """
 
-         if id is not None:
+        if id is not None:
             self.id = id
-         else:
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
