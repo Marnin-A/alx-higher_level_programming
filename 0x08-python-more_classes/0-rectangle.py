5fd8386bfa150eb2ad3df48f14933f51801a179c
Marnin-A <msmaudu@gmail.com>
2024-01-29 20:49:21 -0800

    feat: add witdth property

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -4,5 +4,18 @@ Defines an empty Rectangle class
 """
 
 class Rectangle:
-    """Empty representation of a rectangle"""
+
+     @property
+    def width(self):
+        """getter for the private instance attribute width"""
+        return self.__width
+
+     @width.setter
+    def width(self, value):
+        """setter for the private instance attribute width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
     pass
